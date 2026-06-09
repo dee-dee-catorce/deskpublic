8b02c1483d5d769c14ff31e4888bf7f2681db990
darkkacool-dotcom <darkkacool@gmail.com>
2026-06-09 01:02:53 -0500

    updated popup at start

--- Scripts/impo/main.gd
+++ Scripts/impo/main.gd
@@ -7,9 +7,10 @@ var console: Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if gbData.settings["messageEnabled"] == true:
-		OS.alert("DD14 here \n \n This project is in a VERY EARLY state, expect a lot of weird stuff. \n\n 
+		OS.alert("DD14 here \n \n This project is in a VERY VERY EARLY state, expect a lot of weird stuff. \n\n 
 		 This is an open source project and I encourage you to add your own features by downloading the github repo at https://github.com/dee-dee-catorce. \n\n
-		 I am fine with the distribution of modded copies as long as I am credited and they aren't a virus or something")
+		 I am fine with the distribution of modded copies as long as I am credited and they aren't a virus or something \n\n 
+		 I apologize if this project didnt meet expectations, I promised alot without taking into consideration that this is my 2nd ever godot project")
 	
 	GlobalVariable.console.connect(yeah)
 	pass
